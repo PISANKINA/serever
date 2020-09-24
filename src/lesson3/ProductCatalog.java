3ec7e14c020249e8131b3cd168c459bf4b10e942
PISANKINA <pisankinaa00@mail.ru>
2020-09-24 19:19:14 +0700

    a

--- src/lesson3/ProductCatalog.java
+++ src/lesson3/ProductCatalog.java
@@ -8,26 +8,29 @@ public class ProductCatalog {
 
     public List<Product> getAllProducts(){
         return catalog;
-    }
+    } //метод возвращает список из всех продуктов
 
-    public void createProduct(Product product) throws ProductAlreadyExists {
-        if (catalog.contains(product)) throw new ProductAlreadyExists("Product Already Exists");
+    public void createProduct(Product product) throws ProductAlreadyExists {  //метод сохраняет в справочнике новый продукт
+        if (catalog.contains(product)) throw new ProductAlreadyExists("Product Already Exists"); //Если в справочнике есть
+                                                                                                //продукт с таким же именем, выбросить проверяемое исключение
         else catalog.add(product);
     }
 
-    public void updateProduct(Product product) throws ProductNotValid{
+    public void updateProduct(Product product) throws ProductNotValid{ //метод обновляет продукт в справочнике.
         List<Long> idis = new ArrayList<>();
         for (Product catalogProduct : catalog){
             idis.add(catalogProduct.getId());
         }
-        if (idis.contains(product.getId())) {
-            if (product.getName().equals("")) throw new ProductNotValid("Product Now Valid");
-            catalog.add(idis.indexOf(product.getId()), product);
 
+        if (idis.contains(product.getId())) {
+            if (product.getName().equals("")) throw new ProductNotValid("Product Now Valid");//Если в новом продукте не
+                                                                                            //заполнено поле name, выбросить проверяемое исключение
+            catalog.add(idis.indexOf(product.getId()), product);
+        //Если продукт с таким идентификатором не существует, выбросить непроверяемое исключение
         }else throw new ProductNotFound("ProductNotFound");
     }
 
-    public void deleteProduct(long id){
+    public void deleteProduct(long id){  //удаляет продукт из справочника
         boolean isRemoved = false;
         for (Product catalogProduct : catalog){
             if (catalogProduct.getId() == id){
@@ -35,7 +38,8 @@ public class ProductCatalog {
                 isRemoved = true;
             }
         }
-        if (!isRemoved) throw new ProductNotFound("Product Not Found");
+        if (!isRemoved) throw new ProductNotFound("Product Not Found"); //если подходящий продукт в справочнике не найден,
+                                                                        //выбрасывается непроверяемое исключение
         }
     }
 
